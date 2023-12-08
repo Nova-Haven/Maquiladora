37ff04bd89096751f4b8727228c4ae1e080d43f1
Oliasaew <152236369+Oliasaew@users.noreply.github.com>
2023-12-07 20:42:48 -0600

    subir BD
    subí la base de datos de la psicologa

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 require_once "Controladores/ctrl_plantilla.php";
 require_once "Controladores/ctrl_alumno.php";
-require_once "Modelo/mdl_alumno.php";
 require_once "Controladores/ctrl_pantalla.php";
-require_once "Modelo/mdl_pantalla.php";
 $obj_plantilla = new ControladorPlantilla();
 $obj_plantilla -> CtrlPlantilla();
